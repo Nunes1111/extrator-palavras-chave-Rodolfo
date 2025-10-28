33a58d701aec85b7f6a04d31e5d7fa1c8793aacb
Nunes1111 <nunes.rodolfo@escola.pr.gov.br>
2025-10-28 10:14:49 -0300

    28/10

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ campoResultado.textContent = palavrasChave.join(", ")
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    
+    let frequencias = {};
+
+    for(let i of palavras){
+        frequencias[i]=0;
+
+        for (let j of palavras){ 
+            if (i == j){
+                frequencias [i]++;
+            }
+        }
+    }
+
 
     return palavras; 
 
