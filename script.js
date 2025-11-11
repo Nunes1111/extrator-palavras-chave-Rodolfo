a7b9aa2bd2e91500d73aff955ea3e3d27050519a
Nunes1111 <nunes.rodolfo@escola.pr.gov.br>
2025-11-11 10:27:02 -0300

    11./11

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector("#entrada-de-texto").value;
-    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
 
     campoResultado.textContent = palavrasChave.join(", ")
